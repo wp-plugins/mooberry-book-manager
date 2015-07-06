9a835d054eb36f8311fdcdee71a72c9244aafc66
mooberrydreams <mooberrydreams@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 11:34:20 +0000

    flushes rewrites
    git-svn-id: https://plugins.svn.wordpress.org/mooberry-book-manager/tags/2.0.1@1193278 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- book.php
+++ book.php
@@ -5,18 +5,45 @@
 // set the book's excerpt to a portion of the summary	
 add_action('save_post_mbdb_book', 'mbdb_save_excerpt');
 function mbdb_save_excerpt($post_id, $post =null, $update=null) {
-	if (array_key_exists('_mbdb_summary', $_POST) && $_POST['_mbdb_summary']) {
-		$summary = $_POST['_mbdb_summary'];
-	} elseif ( array_key_exists('_mbdb_summary', $_GET) && $_GET['_mbdb_summary']) {
-		$summary = $_GET['_mbdb_summary'];
+	 if ($post == null) {
+		if (array_key_exists('_mbdb_summary', $_POST) && $_POST['_mbdb_summary']) {
+			$summary = $_POST['_mbdb_summary'];
+		} elseif ( array_key_exists('_mbdb_summary', $_GET) && $_GET['_mbdb_summary']) {
+			$summary = $_GET['_mbdb_summary'];
+		} else {
+			$summary = '';
+		}
+	/*	
+		if (array_key_exists('_mbdb_cover', $_POST) && $_POST['_mbdb_cover']) {
+			$cover = $_POST['_mbdb_cover'];
+		} elseif ( array_key_exists('_mbdb_cover', $_GET) && $_GET['_mbdb_cover']) {
+			$cover = $_GET['_mbdb_cover'];
+		} else {
+			$cover = '';
+		}
+		
+		if (array_key_exists('_mbdb_subtitle', $_POST) && $_POST['_mbdb_subtitle']) {
+			$subtitle = $_POST['_mbdb_subtitle'];
+		} elseif ( array_key_exists('_mbdb_subtitle', $_GET) && $_GET['_mbdb_subtitle']) {
+			$subtitle = $_GET['_mbdb_subtitle'];
+		} else {
+			$subtitle = '';
+		}
+	*/
 	} else {
-		return;
+		$summary = get_post_meta($post_id, '_mbdb_summary', true);
+		//$subtitle = get_post_meta($post_id, '_mbdb_subtitle', true);
+		//$cover = get_post_meta($post_id, '_mbdb_cover', true);
 	}
+	
+	//$excerpt = '<div class="mbm-archive"><h3 class="mbm-archive-subtitle">' . $subtitle . '</h3><span class="mbm-archive-cover"><img class="mbm-archive-image" src="' . $cover . '"></span><span class="mbm-archive-summary">' . $summary . '<p><A class="mbm-archive-link" HREF="' . esc_url(get_permalink($post_id)) . '"> LEARN MORE </a></p></span></div><p style="clear:both"></p>';
+ 
+
 	// unhook this function so it doesn't loop infinitely
 	remove_action( 'save_post_mbdb_book', 'mbdb_save_excerpt' );
 
 	// update the post, which calls save_post again
-	wp_update_post( array( 'ID' => $post_id, 'post_excerpt' =>  substr($summary, 0, 50)) );
+	wp_update_post( array( 'ID' => $post_id, 'post_excerpt' =>  $summary) );
 
 	// re-hook this function
 	add_action( 'save_post_mbdb_book', 'mbdb_save_excerpt' );	
@@ -35,7 +62,7 @@ function set_up_mbdb_book_columns( $columns ) {
 		'_mbdb_published' => __('Published', 'mooberry-book-manager'),
 		'mbdb_series' => __('Series', 'mooberry-book-manager'),
 		'_mbdb_series_order' => __('Series Order', 'mooberry-book-manager'), 
-		'_mbdb_publisher' => __('Publisher', 'mooberry-book-manager'),
+		'_mbdb_publisherID' => __('Publisher', 'mooberry-book-manager'),
 		'date' => __('Updated', 'mooberry-book-manager')
 	);
 	return apply_filters('mbdb_book_columns', $columns);
@@ -66,7 +93,11 @@ function populate_mbdb_book_columns($column, $post_id) {
 				/* translators: short date format. see http://php.net/date */
 				echo apply_filters('mbdb_book_mbdb_published_column', date(__('m/d/Y'),strtotime($mbdb_published)), $post_id);
 			}
-			do_action('mbdb_book_afteR_mbdb_published_column');
+			do_action('mbdb_book_after_mbdb_published_column');
+			break;
+		case '_mbdb_publisherID':
+			$publisher = mbdb_get_publisher_info(get_post_meta( $post_id, $column, true));
+			echo apply_filters('mbdb_book' . $column . '_column', $publisher['name']);
 			break;
 		default:
 			do_action('mbdb_book_before' . $column . '_column');
@@ -115,21 +146,49 @@ function sort_mbdb_book_columns( $vars) {
 	return $vars;
 } */
 	
+// reorder taxonomy boxes
+add_action('add_meta_boxes_mbdb_book', 'mbdb_reorder_taxonomy_boxes');
+function mbdb_reorder_taxonomy_boxes() {
+	
+    global $wp_meta_boxes;
+	$taxonomies = array(  'tagsdiv-mbdb_tag', 'tagsdiv-mbdb_series', 'tagsdiv-mbdb_genre' );
+	// remove the cover to be readded before the taxonomies
+	$cover = $wp_meta_boxes['mbdb_book']['side']['default']['mbdb_cover_image'];
+	unset($wp_meta_boxes['mbdb_book']['side']['default']['mbdb_cover_image']);
+	foreach ($taxonomies as $taxID) {
+		 $tax = $wp_meta_boxes['mbdb_book']['side']['core'][$taxID];
+		 unset($wp_meta_boxes['mbdb_book']['side']['core'][$taxID]);
+		 if (array_key_exists('default', $wp_meta_boxes['mbdb_book']['side'])) {
+			$wp_meta_boxes['mbdb_book']['side']['default'] = array($taxID => $tax) + $wp_meta_boxes['mbdb_book']['side']['default'];
+		 } else {
+			$wp_meta_boxes['mbdb_book']['side']['default'] = array($taxID => $tax);
+		 }
+	}
+	
+	// now add cover above the taxonomies
+	$wp_meta_boxes['mbdb_book']['side']['default'] = array('mbdb_cover_image' => $cover) + $wp_meta_boxes['mbdb_book']['side']['default'];
+}
+
+
 	
 add_action('add_meta_boxes_mbdb_book', 'mbdb_mbd_metabox', 10);
 function mbdb_mbd_metabox() {
-		add_meta_box('mbdb_mbd_metabox', 'Like Mooberry Book Manager?', 'mbdb_display_mbdb_metabox', 'mbdb_book', 'side', 'high');
+		add_meta_box('mbdb_mbd_metabox', 'Need help with Mooberry Book Manager?', 'mbdb_display_mbdb_metabox', 'mbdb_book', 'side', 'core');
 }
 
 function mbdb_display_mbdb_metabox($post, $args) {
-	echo '<p>Check out <a target="_new" href="http://www.mooberrydreams.com/">our website</a> to learn more about the available add-ons so Mooberry Book Manager can save you more time!</p><h4>Need help with Mooberry Book Manager?</h4>
-		<p><a target="_new" href="http://www.mooberrydreams.com/wp/wp-content/uploads/2015/02/Mooberry-Book-Manager-User-Manual-v1.0.pdf">Download the User Manual</a></p>';
-	echo '<img style="width:225px" src="' . plugins_url('/views/images/logo.png', __FILE__) . '">';
+//	echo '<p>Check out <a target="_new" href="http://www.mooberrydreams.com/">our website</a> to learn more about the available add-ons so Mooberry Book Manager can save you more time!</p><h4>Need help with Mooberry Book Manager?</h4>
+//		<p><a target="_new" href="http://www.mooberrydreams.com/wp/wp-content/uploads/2015/02/Mooberry-Book-Manager-User-Manual-v1.0.pdf">Download the User Manual</a></p>';
+//	echo '<img style="width:225px" src="' . plugins_url('/views/images/logo.png', __FILE__) . '">';
+include "views/admin-about-mooberry.php";
+
 }
 
 
 add_filter( 'cmb2_meta_boxes', 'mbdb_book_metaboxes', 30 );
 function mbdb_book_metaboxes( array $meta_boxes ) {
+	$mbdb_options = get_option('mbdb_options');
+	
 	$meta_boxes['mbdb_summary_metabox'] = array(
 		'id'            => 'mbdb_summary_metabox',
 		'title'         => __('Summary', 'mooberry-book-manager'),
@@ -157,7 +216,113 @@ function mbdb_book_metaboxes( array $meta_boxes ) {
 			),
 		),
 	);
-	
+	$meta_boxes['mbdb_editions'] = array(
+		'id'            => 'mbdb_editions',
+		'title'         => __('Formats and Editions', 'mooberry-book-manager'),
+		'object_types'  => array( 'mbdb_book', ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'default',
+		
+		'show_names'    => true, // Show field names on the left
+		'fields' => array(
+			array(
+			'id'          => '_mbdb_editions',
+			'type'        => 'group',
+			'description' => __("List the details of your book's hardcover, paperback, and e-book editions. Everything is optional except the format.", 'mooberry-book-manager'),
+			'options'     => array(
+				'group_title'   => __('Edition', 'mooberry-book-manager') . ' {#}', // {#} gets replaced by row number
+				'add_button'    =>  __('Add New Edition', 'mooberry-book-manager'),
+				'remove_button' =>  __('Remove Edition', 'mooberry-book-manager'),
+				'sortable'      => false, // beta
+				),
+			
+				// Fields array works the same, except id's only need to be unique for this group. Prefix is not needed.
+				'fields'      => array(
+					array(
+						'name'	=>	_x('Format', 'noun', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_format',
+						'type'	=>	'select',
+						'sanitization_cb' => 'mbdb_validate_editions', 
+						'options'	=> mbdb_get_editions(),
+					),
+					array(
+						'name'	=> __('EAN/ISBN', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_isbn',
+						'type'	=>	'text_medium',
+					),
+					array(
+						'name'	=> __('Language', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_language',
+						'type'	=> 'select',
+						'options'	=> mbdb_get_language_array(),
+						'default'	=>	mbdb_get_default_language($mbdb_options),
+					),
+					array(
+						'name'	=> __('Number of Pages', 'mooberry-book-manager'),
+						'id'	=> '_mbdb_length',
+						'type'	=> 'text_small',
+						'attributes' => array(
+								'type' => 'number',
+								'pattern' => '\d*',
+								'min' => 1
+							),
+					),
+					array(
+						'name'	=>	__('Height', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_height',
+						'type'	=>	'text_small',
+						'attributes' => array(
+							'type' => 'number',
+							'step' => 'any',
+							'min' => 0
+						),
+					),
+					array(
+						'name'	=>	__('Width', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_width',
+						'type'	=> 'text_small',
+						'attributes' => array(
+							'type' => 'number',
+							'step' => 'any',
+							'min' => 0
+						),
+					),
+					array(
+						'name'	=> _x('Unit', 'units of measurement', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_unit',
+						'type'	=> 'select',
+						'options'	=> mbdb_get_units_array(),
+						'default'	=>	mbdb_get_default_unit($mbdb_options),
+					),
+					array(
+						'name'	=>	__('Suggested Retail Price', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_retail_price',
+						'type'	=> 'text_small',
+						'attributes' => array(
+								'pattern' => '^\d*([.,]\d{2}$)?',
+								'min' => 0
+							),
+						
+					),
+					array(	
+						'name'	=>	__('Currency', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_currency',
+						'type'	=>	'select',
+						'options'	=>	mbdb_get_currency_array(),
+						'default'	=> mbdb_get_default_currency($mbdb_options),
+					),
+					array(
+						'name'	=>	__('Edition Title', 'mooberry-book-manager'),
+						'id'	=>	'_mbdb_edition_title',
+						'type'	=> 'text_medium',
+						'desc' => __('First Edition, Second Edition, etc.', 'mooberry-book-mananger'),
+					),
+				),
+			),
+		),
+	);
+				
+				
 	$meta_boxes['mbdb_excerpt'] = array(
 		'id'            => 'mbdb_excerpt',
 		'title'         => __('Excerpt', 'mooberry-book-manager'),
@@ -198,7 +363,7 @@ function mbdb_book_metaboxes( array $meta_boxes ) {
 			array(
 			'id'          => '_mbdb_reviews',
 			'type'        => 'group',
-			'description' => 'Add reviews of your book',
+			'description' => __('Add reviews of your book', 'mooberry-book-manager'),
 			'options'     => array(
 				'group_title'   => _x('Reviews', 'noun', 'mooberry-book-manager') . ' {#}', // {#} gets replaced by row number
 				'add_button'    =>  __('Add Review', 'mooberry-book-manager'),
@@ -240,11 +405,102 @@ function mbdb_book_metaboxes( array $meta_boxes ) {
 		),
 	);
 	
+		$meta_boxes['mbdb_cover_image'] = array(
+		'id'            => 'mbdb_cover_image',
+		'title'         => _x('Cover', 'noun', 'mooberry-book-manager'),
+		'object_types'  => array( 'mbdb_book', ), // Post type
+		'context'       => 'side',
+		'priority'      => 'default',
+			
+		'show_names'    => false, // Show field names on the left
+		'allow'			=> array( 'attachment'),
+		'fields' => array(
+			array(
+				 'name' => _x('Book Cover', 'noun', 'mooberry-book-manager'),
+				'id' => '_mbdb_cover',
+				'type' => 'file',
+				'allow' => array(  'attachment' ) // limit to just attachments with array( 'attachment' )
+			),
+		),
+	);
+	
+	$meta_boxes['mbdb_bookinfo_metabox'] = array(
+		'id'            => 'mbdb_bookinfo_metabox',
+		'title'         => __('Book Details', 'mooberry-book-manager'),
+		'object_types'  => array( 'mbdb_book', ), // Post type
+		'context'       => 'side',
+		'priority'      => 'default',
+			
+		'show_names'    => true, // Show field names on the left
+		'fields' => array(
+			array(
+				'name' => __('Subtitle', 'mooberry-book-manager'),
+				'id'   => '_mbdb_subtitle',
+				'type' => 'text_small',
+			),
+			array(
+				'name' 	=> __('Release Date', 'mooberry-book-manager'),
+				'id'	=> '_mbdb_published',
+				'type' => 'text_date',
+				'desc' => 'yyyy/mm/dd',
+				'date_format' => 'Y/m/d',
+				'sanitization_cb' => 'mbdb_format_date'
+			),
+		/*	array(
+				'name' => __('Publisher', 'mooberry-book-manager'),
+				'id'   => '_mbdb_publisher',
+				'type' => 'text_medium',
+			),
+			array(
+				'name' 	=> __('Publisher Website', 'mooberry-book-manager'),
+				'id'	=> '_mbdb_publisherwebsite',
+				'type'	=> 'text_url',
+				'desc' => 'http://www.someWebsite.com/',
+				'attributes' =>  array(
+					//'pattern' => '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})\/?([\/\w \.=\?&\-%]*)*\/?',
+					'pattern' => '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6}).*',
+					
+				),
+			), */
+			array(
+				'name' => __('Publisher', 'mooberry-book-manager'),
+				'id'   => '_mbdb_publisherID',
+				'type' => 'select',
+				'options' => 'mbdb_get_publishers',
+				'desc' 	=> __('Set up Publishers in Settings.', 'mooberry-book-manager'),
+			),
+			array(
+				'name'	=> __('Goodreads Link', 'mooberry-book-manager'),
+				'id'	=> '_mbdb_goodreads',
+				'type'	=> 'text_url',
+				'desc' => 'http://www.goodreads.com/your/Unique/Text/',
+				'attributes' =>  array(
+				//	'pattern' => '^(https?:\/\/)?www.goodreads.com([\/\w \.=\?&\-]*)*\/?',
+					'pattern' => '^(https?:\/\/)?www.goodreads.com.*',
+				),
+			),
+			
+			array(
+				'name'	=> __('Series Order', 'mooberry-book-manager'),
+				'id'	=> '_mbdb_series_order',
+				'desc'	=> __('(leave blank if not part of a series)', 'mooberry-book-manager'),
+				'type'	=> 'text_small',
+				'attributes' => array(
+						'type' => 'number',
+						//'pattern' => '\d*.?\d*',
+						'step' => 'any',
+						'min' => 0
+					),
+			),
+		),
+	);
+	
+
 	$meta_boxes['mbdb_buylinks_metabox'] = array(
 		'id'            => 'mbdb_buylinks_metabox',
 		'title'         => _x('Retailer Links', 'noun', 'mooberry-book-manager'),
 		'object_types'  => array( 'mbdb_book', ), // Post type
-		'context'       => 'normal',
+		'context'       => 'side',
 		'priority'      => 'default',
 			
 		
@@ -327,103 +583,9 @@ function mbdb_book_metaboxes( array $meta_boxes ) {
 		),
 	);
 	
-	$meta_boxes['mbdb_bookinfo_metabox'] = array(
-		'id'            => 'mbdb_bookinfo_metabox',
-		'title'         => __('Book Details', 'mooberry-book-manager'),
-		'object_types'  => array( 'mbdb_book', ), // Post type
-		'context'       => 'side',
-		'priority'      => 'low',
-			
-		'show_names'    => true, // Show field names on the left
-		'fields' => array(
-			array(
-				'name' => __('Subtitle', 'mooberry-book-manager'),
-				'id'   => '_mbdb_subtitle',
-				'type' => 'text_small',
-			),
-			array(
-				'name' => __('Publisher', 'mooberry-book-manager'),
-				'id'   => '_mbdb_publisher',
-				'type' => 'text_medium',
-			),
-			array(
-				'name' 	=> __('Publisher Website', 'mooberry-book-manager'),
-				'id'	=> '_mbdb_publisherwebsite',
-				'type'	=> 'text_url',
-				'desc' => 'http://www.someWebsite.com/',
-				'attributes' =>  array(
-					//'pattern' => '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})\/?([\/\w \.=\?&\-%]*)*\/?',
-					'pattern' => '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6}).*',
-					
-				),
-			),
-			array(
-				'name' 	=> __('Release Date', 'mooberry-book-manager'),
-				'id'	=> '_mbdb_published',
-				'type' => 'text_date',
-				'desc' => 'yyyy/mm/dd',
-				'date_format' => 'Y/m/d',
-				'sanitization_cb' => 'mbdb_format_date'
-			),
-			array(
-				'name'	=> __('Goodreads Link', 'mooberry-book-manager'),
-				'id'	=> '_mbdb_goodreads',
-				'type'	=> 'text_url',
-				'desc' => 'http://www.goodreads.com/your/Unique/Text/',
-				'attributes' =>  array(
-				//	'pattern' => '^(https?:\/\/)?www.goodreads.com([\/\w \.=\?&\-]*)*\/?',
-					'pattern' => '^(https?:\/\/)?www.goodreads.com.*',
-				),
-			),
-			array(
-				'name'	=> __('Number of Pages', 'mooberry-book-manager'),
-				'id'	=> '_mbdb_length',
-				'type'	=> 'text_small',
-				'attributes' => array(
-						'type' => 'number',
-						'pattern' => '\d*',
-						'min' => 1
-					),
-			),
-			array(
-				'name'	=> __('Series Order', 'mooberry-book-manager'),
-				'id'	=> '_mbdb_series_order',
-				'desc'	=> __('(leave blank if not part of a series)', 'mooberry-book-manager'),
-				'type'	=> 'text_small',
-				'attributes' => array(
-						'type' => 'number',
-						//'pattern' => '\d*.?\d*',
-						'step' => 'any',
-						'min' => 0
-					),
-			),
-		),
-	);
 	
-	$meta_boxes['mbdb_cover_image'] = array(
-		'id'            => 'mbdb_cover_image',
-		'title'         => _x('Cover', 'noun', 'mooberry-book-manager'),
-		'object_types'  => array( 'mbdb_book', ), // Post type
-		'context'       => 'side',
-		'priority'      => 'low',
-			
-		'show_names'    => false, // Show field names on the left
-		'allow'			=> array( 'attachment'),
-		'fields' => array(
-			array(
-				 'name' => _x('Book Cover', 'noun', 'mooberry-book-manager'),
-				'id' => '_mbdb_cover',
-				'type' => 'file',
-				'allow' => array(  'attachment' ) // limit to just attachments with array( 'attachment' )
-			),
-		),
-	);
 
 	
 	return apply_filters('mbdb_book_meta_boxes', $meta_boxes);
 	
 }
-	
-
-
-?>
--- includes/js/admin-book-grid.js
+++ includes/js/admin-book-grid.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('load', onLoad, false);
 
 function onLoad() {
@@ -7,80 +8,79 @@ function onLoad() {
 	}
 		
 	if (document.getElementById('_mbdb_book_grid_books') != null) {
-		document.getElementById('_mbdb_book_grid_books').addEventListener('change', booksChange, false);
-		booksChange();
+		document.getElementById('_mbdb_book_grid_books').addEventListener('change', displayChange, false);
 	}
 	
 	
 	if (document.getElementById('_mbdb_book_grid_group_by') != null) {
-		document.getElementById('_mbdb_book_grid_group_by').addEventListener('change', groupByChange, false);
-		groupByChange();
+		document.getElementById('_mbdb_book_grid_group_by').addEventListener('change', displayChange, false);
 	}
 	
 	if (document.getElementById('_mbdb_book_grid_genre_group_by') != null) {
-		document.getElementById('_mbdb_book_grid_genre_group_by').addEventListener('change', genreGroupByChange, false);
-		genreGroupByChange();
+		document.getElementById('_mbdb_book_grid_genre_group_by').addEventListener('change', displayChange, false);
+	}
+	/*
+	if (document.getElementById('_mbdb_book_grid_cover_height_default') != null) {
+		document.getElementById('_mbdb_book_grid_cover_height_default').addEventListener('change', displayChange, false);
+	}
+	*/
+	if (document.getElementById('_mbdb_book_grid_books_across_default') != null) {
+		document.getElementById('_mbdb_book_grid_books_across_default').addEventListener('change', displayChange, false);
 	}
 }
+
+
+	function toggle_display(surroundingElementClass, dropdownID, childElementClass, dropdownValue, shouldDisplay) {
+		var dropdown = document.getElementById(dropdownID);
+		var surroundingElement = document.getElementsByClassName(surroundingElementClass);
+		var childElement =  document.getElementsByClassName(childElementClass);
+		
+		var match = (shouldDisplay ? 'block' : 'none');
+		var nomatch = (!shouldDisplay ? 'block' : 'none');
+		
+		if (surroundingElement[0].style.display == 'block') {
+			if ( dropdown.value == dropdownValue) {
+				childElement[0].style.display = match;
+			} else {
+				childElement[0].style.display = nomatch;
+			}
+		}
+	}
+	
+	
+	function booksAcrossChange() {		
+		toggle_display('cmb2-id--mbdb-book-grid-books-across-default', '_mbdb_book_grid_books_across_default', 'cmb2-id--mbdb-book-grid-books-across', 'yes', false);
+	}
+/*	
+function coverHeightChange() {
+		toggle_display('cmb2-id--mbdb-book-grid-cover-height-default', '_mbdb_book_grid_cover_height_default', 'cmb2-id--mbdb-book-grid-cover-height', 'yes', false);
+	}
+*/
+	
 	
 	function genreGroupByChange() {
-		var genre_group_by = document.getElementById('_mbdb_book_grid_genre_group_by');
-		var grid_order =  document.getElementsByClassName('cmb2-id--mbdb-book-grid-order');
 		
-		if (genre_group_by.style.display=='block' && genre_group_by.value == 'series') {
-			grid_order[0].style.display = 'none';
-		} else {
-			grid_order[0].style.display = 'block';
-			
-		}
+		toggle_display('cmb2-id--mbdb-book-grid-genre-group-by', '_mbdb_book_grid_genre_group_by', 'cmb2-id--mbdb-book-grid-order', 'series', false);
+		
 		
 	}
 
 	
 	function groupByChange() {
-		var group_by = document.getElementById('_mbdb_book_grid_group_by');
-		var genre_group_by = document.getElementsByClassName('cmb2-id--mbdb-book-grid-genre-group-by');
-		var grid_order =  document.getElementsByClassName('cmb2-id--mbdb-book-grid-order');
 		
-		if (group_by.value == 'genre') {
-			genre_group_by[0].style.display = 'block';
-		} else {
-			genre_group_by[0].style.display = 'none';
-		}
+		toggle_display('cmb2-id--mbdb-book-grid-group-by', '_mbdb_book_grid_group_by', 'cmb2-id--mbdb-book-grid-genre-group-by', 'genre', true);
+		toggle_display('cmb2-id--mbdb-book-grid-group-by', '_mbdb_book_grid_group_by', 'cmb2-id--mbdb-book-grid-order', 'series', false);
+	//	genreGroupByChange();
 		
-		if (group_by.style.display=='block' && group_by.value == 'series') {
-			grid_order[0].style.display = 'none';
-		} else {
-			grid_order[0].style.display = 'block';
-		}
+		
 		
 	}
 	
 	function booksChange() {
-		var books_select = document.getElementById('_mbdb_book_grid_books');
-		var books_checkboxes = document.getElementsByClassName('cmb2-id--mbdb-book-grid-custom-select');
-		var genre_checkboxes = document.getElementsByClassName('cmb2-id--mbdb-book-grid-genre');
-		var series_checkboxes = document.getElementsByClassName('cmb2-id--mbdb-book-grid-series');
-	
+		toggle_display('cmb2-id--mbdb-book-grid-books', '_mbdb_book_grid_books', 'cmb2-id--mbdb-book-grid-custom-select', 'custom', true);
+		toggle_display('cmb2-id--mbdb-book-grid-books', '_mbdb_book_grid_books', 'cmb2-id--mbdb-book-grid-genre', 'genre', true);
+		toggle_display('cmb2-id--mbdb-book-grid-books', '_mbdb_book_grid_books', 'cmb2-id--mbdb-book-grid-series', 'series', true);
 		
-		
-		if (books_select.value == 'custom') {
-			books_checkboxes[0].style.display = 'block';
-		} else {
-			books_checkboxes[0].style.display = 'none';
-		}
-		if (books_select.value == 'genre') {
-			genre_checkboxes[0].style.display = 'block';
-		
-		} else {
-			genre_checkboxes[0].style.display = 'none';
-		}
-		if (books_select.value == 'series') {
-			series_checkboxes[0].style.display = 'block';
-		
-		} else {
-			series_checkboxes[0].style.display = 'none';
-		}
 	}
 	
 	function displayChange() {
@@ -92,64 +92,35 @@ function onLoad() {
 		var series_checkboxes = document.getElementsByClassName('cmb2-id--mbdb-book-grid-series');
 		var group_by = document.getElementsByClassName('cmb2-id--mbdb-book-grid-group-by');
 		var genre_group_by = document.getElementsByClassName('cmb2-id--mbdb-book-grid-genre-group-by');
-		var book_grid_cover_size = document.getElementsByClassName('cmb2-id--mbdb-book-grid-cover-height');
+	//	var book_grid_cover_size = document.getElementsByClassName('cmb2-id--mbdb-book-grid-cover-height');
 		var book_grid_books_across = document.getElementsByClassName('cmb2-id--mbdb-book-grid-books-across');
+	//	var cover_height_default = document.getElementsByClassName('cmb2-id--mbdb-book-grid-cover-height-default');
+		var books_across_default = document.getElementsByClassName('cmb2-id--mbdb-book-grid-books-across-default');
 
+		
 		if (display_grid.value == 'yes') {
 			books_select[0].style.display = 'block';
 			group_by[0].style.display = 'block'
-			book_grid_cover_size[0].style.display = 'block';
-			book_grid_books_across[0].style.display = 'block';
+		//	cover_height_default[0].style.display = 'block';
+			books_across_default[0].style.display = 'block';
 			booksChange();
 			groupByChange();
 			genreGroupByChange();
-			// books_select.removeAttribute("disabled");
-			// grid_order.removeAttribute("disabled");
-			// book_grid_cover_size.removeAttribute("disabled");
-			// book_grid_books_across.removeAttribute("disabled");
-			// group_by.removeAttribute("disabled");
-			
-			// if (books_checkboxes[0] != null) {
-				// books_checkboxes[0].removeAttribute("disabled");
-			// }
-			// if (genre_checkboxes[0] != null) {
-				// genre_checkboxes[0].removeAttribute("disabled");
-			// }
-			// if (series_checkboxes[0] != null) {
-				// series_checkboxes[0].removeAttribute("disabled");
-			// }
-			
-			// if (genre_group_by != null) {
-				// genre_group_by.removeAttribute("disabled");
-			// }
+		//	coverHeightChange();
+			booksAcrossChange();
 			
 		} else {
 			books_select[0].style.display = 'none';
 			group_by[0].style.display = 'none'
-			book_grid_cover_size[0].style.display = 'none';
+		//	book_grid_cover_size[0].style.display = 'none';
 			book_grid_books_across[0].style.display = 'none';
 			books_checkboxes[0].style.display = 'none';
 			genre_checkboxes[0].style.display = 'none';
 			series_checkboxes[0].style.display = 'none';
 			genre_group_by[0].style.display  = 'none';
 			grid_order[0].style.display = 'none';
-			// books_select.setAttribute("disabled", true);
-			// grid_order.setAttribute("disabled", true);
-			// book_grid_cover_size.setAttribute("disabled", true);
-			// book_grid_books_across.setAttribute("disabled", true);
-			// group_by.setAttribute("disabled", true);
-			// if (books_checkboxes[0] != null) {
-				// books_checkboxes[0].setAttribute("disabled", true);
-			// }
-			// if (genre_checkboxes[0] != null) {
-				// genre_checkboxes[0].setAttribute("disabled", true);
-			// }
-			// if (series_checkboxes[0] != null) {
-				// series_checkboxes[0].setAttribute("disabled", true);
-			// }
-			// if (genre_group_by != null) {
-				// genre_group_by.setAttribute("disabled", true);
-			// }
+		//	cover_height_default[0].style.display = 'none';
+			books_across_default[0].style.display = 'none';
 		
 		}
 		
